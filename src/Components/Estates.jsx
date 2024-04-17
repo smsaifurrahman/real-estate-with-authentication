f303e0eae0800cf9b54f2ba4b96d3adb2a6f5027
smsaifurrahman <smsaifur525@gmail.com>
2024-04-17 08:35:20 +0200

    Final chekcking completed

--- src/Components/Estates.jsx
+++ src/Components/Estates.jsx
@@ -7,7 +7,7 @@ const Estates = ({estates}) => {
     return (
         <div className="mt-4 md:mt-12 lg:mt-16">
             <h1 className="text-2xl md:text-3xl mb-8 text-center font-bold">Discover Your Dream Home: Unveiling Luxury <br /> Living at its Finest</h1>
-            <div className="grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1 gap-8 my-8 animate__animated animate__pulse ">
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1 gap-8 my-8  ">
                 {
                     estates.map(estate => <EstatesCard estate={estate} key={estate.id}></EstatesCard>)
                 }
